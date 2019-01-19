fedda97ae48bb9b5626bd4c225671248cf926917
snow2000 <flo.schneeweis@gmail.com>
2019-01-19 19:03:16 +0100

    Initial commit

---
+++ Schule_Maier_Schneeweis/src/Klasse.java
@@ -0,0 +1,9 @@
+
+public class Klasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Schule_Maier_Schneeweis/src/Schule_Maier_Schneeweis.java
@@ -0,0 +1,9 @@
+
+public class Schule_Maier_Schneeweis {
+
+	public static void main(String[] args) {
+		
+		System.out.println("test");
+	}
+
+}
